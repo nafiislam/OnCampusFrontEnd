4b90f6e6cc9d7c0b1c08d0509bc524876a0fa596
Nahin009 <1905009@ugrad.cse.buet.ac.bd>
2024-01-24 20:23:10 +0600

    sidebar and navbar added

---
+++ app/page.tsx
@@ -0,0 +1,14 @@
+import Button1 from "@/components/Button1";
+import { SidebarWithContentSeparator } from "@/components/sidebar";
+import { NavbarDark } from "@/components/Navbar";
+
+export default function Home() {
+ return(
+  <div className="m-4">
+    <NavbarDark />
+    <SidebarWithContentSeparator />
+ </div>
+ )
+}
+
+
---
+++ components/Navbar.tsx
@@ -0,0 +1,54 @@
+"use client";
+import { BellIcon, Cog6ToothIcon } from "@heroicons/react/24/solid";
+import {
+  Button,
+  IconButton,
+  Input,
+  Navbar,
+  Typography,
+  
+} from "@material-tailwind/react";
+
+import { ProfileMenu } from "./AvatarMenu";
+import { NotificationsMenu } from "./NotiMenu";
+
+import { NavbarProps } from "@material-tailwind/react";
+
+import Image from "next/image";
+import Link from "next/link";
+import React from "react";
+
+export function NavbarDark() {
+  return (
+    <Navbar
+          variant="gradient"
+          color="blue-gray"
+          className="max-w-full from-blue-gray-900 to-blue-gray-600 px-4 py-3"   placeholder={undefined}    >
+      <div className="flex flex-wrap items-center justify-between gap-y-4 text-white">
+        <Link href={"#"}><Image src="/images/logo.png" width={50} height={40} alt="logo" className="m-0 p-0 rounded-full" /></Link>
+        <div className="relative flex w-full gap-2 md:w-max ml-8">
+          <Input
+                      type="search"
+                      color="white"
+                      label="Type here..."
+                      className="pr-20"
+                      containerProps={{
+                          className: "min-w-[288px]",
+                      }} crossOrigin={undefined}          />
+          <Button
+                      size="sm"
+                      color="white"
+                      className="!absolute right-1 top-1 rounded"   placeholder={undefined}          >
+            Search
+          </Button>
+        </div>
+        <div className="ml-auto flex gap-8 md:mr-4">
+          <NotificationsMenu />
+          
+          <ProfileMenu />
+        </div>
+        
+      </div>
+    </Navbar>
+  );
+}
