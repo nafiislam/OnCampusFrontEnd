cf80fc94a34e8ef80496652086ddb6824af3e636
Nahin009 <1905009@ugrad.cse.buet.ac.bd>
2024-02-10 22:43:13 +0600

    cvdvvdfv

---
+++ components/Events/DummyTypes.tsx
@@ -0,0 +1,52 @@
+const eventTypes = [
+  {
+    id: 1,
+    name: "Workshop",
+  },
+  {
+    id: 2,
+    name: "Seminar",
+  },
+  {
+    id: 3,
+    name: "Competitions",
+  },
+  {
+    id: 4,
+    name: "Sports",
+  },
+  {
+    id: 5,
+    name: "Shapa-day",
+  },
+  {
+    id: 6,
+    name: "Cultural",
+  },
+  {
+    id: 7,
+    name: "Flashmobs",
+  },
+  {
+    id: 8,
+    name: "Rag-Concert",
+  },
+  {
+    id: 9,
+    name: "Picnic",
+  },
+  {
+    id: 10,
+    name: "Tour",
+  },
+  {
+    id:11,
+    name: "Normal Online Event",
+  },
+  {
+    id:12,
+    name: "Normal Offline Event",
+  }
+];
+
+export { eventTypes };
